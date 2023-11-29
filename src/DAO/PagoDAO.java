35adebff10693e83a87c0c54b381c9c32497fb50
niltonbonifacio2004 <152344003+niltonbonifacio2004@users.noreply.github.com>
2023-11-28 20:45:39 -0500

    Update PagoDAO.java
    Nilton

--- src/DAO/PagoDAO.java
+++ src/DAO/PagoDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
 import Modelo.Pago;
@@ -14,46 +10,46 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-public class PagoDAO {
-
+public class PagoDao {
+    
     private static final String PAGOS_FILE = "src/com/archivos_txt/pagos.txt";
     private List<Pago> pagos;
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     private static int contadorID = 1;
-
-    public PagoDAO() {
+    
+    public PagoDAO () {
         pagos = new ArrayList<>();
         ArchivoUtil.crearCarpeta(PAGOS_FILE);
-        cargarPagosDesdeArchivo();
+        cargarPagosDedeArchivo();
     }
 
     private void cargarPagosDesdeArchivo() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(PAGOS_FILE))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(PAGOS_FILe))) {
             String linea;
-            while ((linea = reader.readLine()) != null) {
+            while ((linea = reader.readLine()) !=null) {
                 String[] partes = linea.split(",");
-                String idPago = partes[0];
+                String idpago = partes [0];
                 Date fechaPago = dateFormat.parse(partes[1]);
                 double monto = Double.parseDouble(partes[2]);
-
-                // Crear un objeto Pago
+                
+                //Crear un objeto Pago
                 Pago pago = new Pago(idPago, fechaPago, monto);
 
-                // Agregar el pago a la lista
-                pagos.add(pago);
+                //Agregar el pago a la lista
+                pagos.add(pago);              
             }
-        } catch (IOException | ParseException e) {
+        }catch (IOException | ParseException e) {
             e.printStackTrace();
         }
     }
 
     public Pago obtenerPagoPorId(String idPago) {
         for (Pago pago : pagos) {
-            if (pago.getIdPago().equals(idPago)) {
+            if (pago.getIdPago():equals(idPago)) {
                 return pago;
             }
         }
-        return null; // Pago no encontrado
+        return null;// Pago no encontrado
     }
 
     public void crearPago(Pago pago) {
@@ -61,24 +57,25 @@ public class PagoDAO {
         pagos.add(pago);
         guardarPagosEnArchivo();
     }
-    
-     public String generarId() {
-        String prefijo = "CODP";
+
+    public String generarId() {
+        String prefijo = "COPD";
         Random random = new Random();
-        int numeroAleatorio = random.nextInt(10000); 
+        int numeroAleatorio = random.nextInt(10000);
         return prefijo + String.format("%04d", numeroAleatorio);
     }
 
-    // Método para guardar pagos en el archivo
+    //Metodo para guardar pagos en el archivo
     public void guardarPagosEnArchivo() {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(PAGOS_FILE))) {
+        try (PrintWriter writer = new PrintWriter (new FileWriter(PAGOS_FILE))) {
             for (Pago pago : pagos) {
                 String fechaPagoStr = dateFormat.format(pago.getFechaPago());
                 writer.println(pago.getIdPago() + "," + fechaPagoStr + "," + pago.getMonto());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }catch (IOexeption e) {
+            e.printStacktrace();
         }
     }
-
 }
+                
+
